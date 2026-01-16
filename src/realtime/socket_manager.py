db1ad756ef88b778f4fc59d45a7460bc9a84207d
Hao Bui <bhao090306@gmail.com>
2026-01-16 14:19:21 +0700

    final fix

--- src/realtime/socket_manager.py
+++ src/realtime/socket_manager.py
@@ -1,9 +1,11 @@
 from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(
+    cors_allowed_origins="*",
+    async_mode="threading"
+)
 
 
 def send_order_to_kitchen(order_data: dict):
-
     socketio.emit("new_order", order_data)
     print("📡 Sent order to kitchen via WebSocket:", order_data)
